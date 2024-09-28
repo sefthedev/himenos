3a573b84b3b684085643018457de11725107ba79
sefthedev <sefer.ekmekci@outlook.com>
2024-09-28 14:12:29 +0200

    refactor: Change package names from kady to himenos

--- himenos-gateway/src/main/java/com/himenos/gateway/GatewayApplication.java
+++ himenos-gateway/src/main/java/com/himenos/gateway/GatewayApplication.java
@@ -1,11 +1,7 @@
-package com.kady.gateway;
+package com.himenos.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
--- himenos-gateway/src/test/java/com/himenos/gateway/GatewayApplicationTests.java
+++ himenos-gateway/src/test/java/com/himenos/gateway/GatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kady.gateway;
+package com.himenos.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
